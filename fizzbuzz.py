e59db0ac9554cb0bf455349b7b86b52ceeb0174e
codingkaty <55514282+codingkaty@users.noreply.github.com>
2022-04-26 14:38:10 -0700

    add fizzbuzz via upload

---
+++ fizzbuzz.py
@@ -0,0 +1,17 @@
+#fizzbuzz project 
+
+i = 1
+
+while i < 100:
+    if i%3==0 and i%5==0:
+        print("this is fizzbuzz {}".format(i))
+    elif i%3 == 0:
+        # print(i)
+        # print(i + "fizz")
+        print("this is fizz {}".format(i))
+    elif i%5 == 0:
+        print(i)
+        print("this is buzz {}".format(i))
+    else:
+        print(i)
+    i += 1
